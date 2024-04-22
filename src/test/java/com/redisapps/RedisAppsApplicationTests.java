b93600fad585556296a8cbe0d0a22257ffd9a018
Hoon <53715807+totohoon02@users.noreply.github.com>
2024-04-22 13:19:27 +0900

    project init
    redis properties 추가
    의존성 추가

---
+++ src/test/java/com/redisapps/RedisAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redisapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
